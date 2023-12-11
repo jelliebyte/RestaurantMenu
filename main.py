b8b77223f261283ff3eb549910852f2a54de2227
jelliebyte <147528422+jelliebyte@users.noreply.github.com>
2023-12-11 16:01:03 +0000

    added comments that explain code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ total = 0
 menuList = ["1.Burger:", "2.Pizza:", "3.Hotdog:"]  # 0-2
 menuPrices = [6, 10, 4]
 foodSizes = ["Small:", "Medium:", "Large:"]
-sizeAddOn = [0, 1, 3]
+sizeAddOn = [0, 1, 3]  #oops gotta finish
 sizeGet = 0
 choice = 0
 print("Generic Resturant Menu")
@@ -10,23 +10,27 @@ print("Generic Resturant Menu")
 
 def printMenu():
   print("*******************************")
-  for i in range(len(menuList)):
-    print(menuList[i], menuPrices[i])
+  for i in range(len(menuList)):  #iterates through the length of the list
+    print(menuList[i], menuPrices[i])  #print each item in the lists
   for i in range(len(foodSizes)):
-    print(foodSizes[i], str(sizeAddOn[i]) + "{}".format("$"))
+    print(foodSizes[i],
+          str(sizeAddOn[i]) +
+          "{}".format("$"))  #print the sizes and the dollar amount
   print("*******************************")
 
 
 def chooseFood(choice):
-  while choice not in range(1, 4):
+  while choice not in range(1, 4):  #need to fix loop D:
     printMenu()
     choice = int(input("What would you like: "))
-    print(menuList[choice - 1], end="")
-    print(str(menuPrices[choice - 1]) + "{}".format("$"))
+    print(menuList[choice - 1], end="")  #prints our option
+    print(
+        str(menuPrices[choice - 1]) + "{}".format("$")
+    )  #prints the price and adds a $ by formatting (probably could've used an fstring)
   return choice
 
 
-def getTotal():
+def getTotal(): #calculates total
   global total
   print("*******************************")
   quantity = int(input("How many would you like?: "))
