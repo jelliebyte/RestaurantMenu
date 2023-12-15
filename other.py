44d23b723035de380d4dbf483ac471a14a6fcecc
jelliebyte <147528422+jelliebyte@users.noreply.github.com>
2023-12-15 16:55:05 +0000

    refactored most of it

--- other.py
+++ other.py
@@ -1,10 +1,9 @@
 total = 0
-menuList = ["1.Burger:", "2.Pizza:", "3.Hotdog:"]  # 0-2
 menuPrices = [6, 10, 4]
+menuList = ["1.Burger:", "2.Pizza:", "3.Hotdog:"]
 foodSizes = ["Small:", "Medium:", "Large:"]
 sizeAddOn = [0, 1, 3]  #oops gotta finish
 sizeGet = 0
-choice = 0
 e = "$"
 print("Generic Resturant Menu")
 
@@ -18,28 +17,40 @@ def printMenu():
           str(sizeAddOn[i]) + f"{e}")  #print the sizes and the dollar amount
   print("*******************************")
 
-
-def chooseFood(choice):
-  while choice not in range(1, 4):  #need to fix loop D:
-    printMenu()
-    choice = int(input("What would you like: "))
-    print(menuList[choice - 1], end="")  #prints our option
-    print(
-        str(menuPrices[choice - 1]) + f"{e}")
-  return choice
-
+def chooseFood():
+  global choice
+  global tax
+  choice = int(input())
+  match choice:
+    case 1:
+      choice = 6
+      tax = 0.75
+      print(f"{menuList[0]} {menuPrices[0]} ".strip() + e)
+      
+    case 2:
+      choice = 10
+      tax = 1.75
+      print(f"{menuList[1]}, {menuPrices[1]}".strip() + e)
+    case 3:
+      choice = 4
+      tax = 0.45
+      print(f"{menuList[2]}, {menuPrices[2]}".strip() + e)
+    case _:
+      print("Not a valid item!")
+      printMenu()
+      chooseFood()
+      print(" ")
 
 def getTotal():  #calculates total
   global total
-  tax = 1.75
   print("*******************************")
   quantity = int(input("How many would you like?: "))
-  subtotal = menuPrices[choice - 1] * quantity
+  subtotal = choice * quantity
   print(f"Your subtotal is {subtotal}")
   print(tax)
-  total = menuPrices[choice - 1] * quantity + tax
+  total = choice * quantity + tax
   print(f"So your total is: {total}")
 
-
-choice = chooseFood(choice)
+printMenu()
+chooseFood()
 getTotal()
